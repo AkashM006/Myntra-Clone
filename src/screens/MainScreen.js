fdea1aa6cf6b6db75a4ba90b7402a6fe406f1e30
Akash M <akashm006@outlook.com>
2023-01-17 19:24:07 +0530

    Added redux persist

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,9 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View } from 'react-native'
+import React from 'react'
 import HomeNavigation from '../navigation/HomeNavigation'
-import { useSelector } from 'react-redux'
 
 const MainScreen = () => {
 
-    const user = useSelector(state => state.user)
-
-    useEffect(() => {
-        console.log("User: ", user)
-    }, [user])
     return (
         <View style={{ flex: 1 }}>
             <HomeNavigation />
